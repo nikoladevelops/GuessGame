86b4f8fe306756da5f1e92757cc3569fab744894
Nikola <nikoladevelops@gmail.com>
2022-06-22 11:29:53 +0300

    Created RemindPunctuationRules scene. Made small design changes.

---
+++ Scenes/RemindRule/RemindPunctuationRules.gd
@@ -0,0 +1,18 @@
+extends "res://Scenes/RemindRule/RemindRule.gd"
+
+func _ready():
+	$RichTextLabel.text="Избери пунктуационно правило:";
+
+func _loadDataFromDb():
+	""" Load data from db """
+	db.query("SELECT * FROM PunctuationRules");
+	for dict in db.query_result:
+		ruleList.add_item(dict["Name"]);
+
+func _on_RuleList_item_selected(index):
+	audioStreamPlayer.play();
+	
+	var currentSelectedGrammarRuleName = ruleList.get_item_text(index);
+	var currentGrammarRuleDetails = db.query("SELECT * FROM PunctuationRules WHERE Name="+"'"+currentSelectedGrammarRuleName+"'");
+	descriptionTextEdit.text = db.query_result[0]["Description"];
+
