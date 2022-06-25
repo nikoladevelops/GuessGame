f4bcb56b737648d5224203e1f2c23fd44c1f5e48
Nikola <nikoladevelops@gmail.com>
2022-06-25 19:15:33 +0300

    Everything works as expected AS LONG AS THERE IS DATA IN THE DB FILE. All games are finished and ready to be exported to .exe files.

--- Scenes/Main.gd
+++ Scenes/Main.gd
@@ -3,17 +3,5 @@ extends Control
 func _on_Timer_timeout():
 	get_tree().reload_current_scene();
 
-func _on_GuessAuthor_questionAnswered():
-	$Timer.start();
-
-func _on_GuessGenre_questionAnswered():
-	$Timer.start();
-	
-func _on_GuessThemes_questionAnswered():
-	$Timer.start();
-	
-func _on_GuessComposition_questionAnswered():
-	$Timer.start();
-
-func _on_QuestionChlenuvane_questionAnswered():
+func startTimer():
 	$Timer.start();
